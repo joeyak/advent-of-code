fab8a85df8583f86d84db958987dd5fae6097e6d
joeyak <joeyak97@gmail.com>
2024-12-03 16:51:56 -0500

    2024 Day 3

--- 2024/day3/main.go
+++ 2024/day3/main.go
@@ -6,7 +6,9 @@ import (
 	"log/slog"
 	"os"
 	"reflect"
+	"regexp"
 	"runtime"
+	"strconv"
 	"strings"
 	"time"
 
@@ -66,13 +68,44 @@ func main() {
 }
 
 func Part1(input string) (any, string, error) {
+	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
 	result := 0
 	debug := ""
+
+	for _, match := range re.FindAllStringSubmatch(input, -1) {
+		num1, _ := strconv.Atoi(match[1])
+		num2, _ := strconv.Atoi(match[2])
+		subResult := num1 * num2
+		result += subResult
+		debug += fmt.Sprintf("%d * %d = %d | %d | %v\n", num1, num2, subResult, result, match)
+	}
+
 	return result, debug, nil
 }
 
 func Part2(input string) (any, string, error) {
+	re := regexp.MustCompile(`do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)`)
 	result := 0
 	debug := ""
+
+	mulEnabled := true
+	for _, match := range re.FindAllStringSubmatch(input, -1) {
+		debug += match[0]
+		if match[0] == "do()" {
+			mulEnabled = true
+		} else if match[0] == "don't()" {
+			mulEnabled = false
+		} else if mulEnabled {
+			num1, _ := strconv.Atoi(match[1])
+			num2, _ := strconv.Atoi(match[2])
+			subResult := num1 * num2
+			result += subResult
+			debug += fmt.Sprintf(" | %d * %d = %d | %d", num1, num2, subResult, result)
+		} else {
+			debug += " | SKIP"
+		}
+		debug += "\n"
+	}
+
 	return result, debug, nil
 }
